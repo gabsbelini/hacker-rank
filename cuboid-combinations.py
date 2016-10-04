4ba6159aa00349280c51bc34f1a696eb026c95e2
Gabriel Belini <gabs_belini@hotmail.com>
2016-10-04 10:23:48 -0300

    Funcionando para x,i,k diferentes

--- cuboid-combinations.py
+++ cuboid-combinations.py
@@ -2,8 +2,8 @@ valores = [int(input()) for x in range(4)]
 lista = []
 x,i,k = valores[0:3]
 for x in range(valores[0]+1):
-    for i in range(valores[0]+1):
-        for k in range(valores[0]+1):
+    for i in range(valores[1]+1):
+        for k in range(valores[2]+1):
             lista.append([x,i,k])
 lista = [l for l in lista if sum(l) != valores[-1]]
 print(lista)
