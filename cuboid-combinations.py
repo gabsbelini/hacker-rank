fe907854b3e5dfc76002ad55f014a3cc616f28b4
Gabriel Belini <gabs_belini@hotmail.com>
2016-10-04 10:12:23 -0300

    ok.

--- cuboid-combinations.py
+++ cuboid-combinations.py
@@ -1,1 +1,9 @@
-valores = 
+valores = [int(input()) for x in range(4)]
+lista = []
+x,i,k = valores[0:3]
+for x in range(valores[0]+1):
+    for i in range(valores[0]+1):
+        for k in range(valores[0]+1):
+            lista.append([x,i,k])
+lista = [l for l in lista if sum(l) != valores[-1]]
+print(lista)
